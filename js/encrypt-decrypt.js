711849ee38303438b5bcf5c36a314c25e90dc92e
Baka-chan <rootelitz@gmail.com>
2017-03-26 12:19:25 +0700

    Update encrypt-decrypt.js

--- js/encrypt-decrypt.js
+++ js/encrypt-decrypt.js
@@ -14,8 +14,8 @@ function encrypt(){ // encrypt URL
 	if ("" != t.value)
 		if(l.http || l.https){
 			var a = Base64.encode(t.value);
-			f.style.display = "block";
-			f.value = "Copy";
+			// f.style.display = "block";
+			// f.value = "Copy";
 			z.value = location.protocol + "//" + location.hostname + "/get/?url=" + a;
 		}
 	else l.http && l.https || (t.value = "", t.placeholder = "URL harus mengandung http:// atau https://", z.value = "");
