48977b2d8417898d31b4e7c354b8392e465958bf
Baka-chan <rootelitz@gmail.com>
2017-03-26 09:56:01 +0700

    Create safelink-timing.js

---
+++ js/safelink-timing.js
@@ -0,0 +1,19 @@
+/*
+ * Code by BakaDzgn
+ */
+
+function startTiming(){
+  getTiming();
+}
+function getTiming(){
+  totalTimeout += 1000;
+  if(totalTimeout =< 6001){
+	  setTimeout(setTiming, totalTimeout);
+  }
+}
+function setTiming(){
+	totalSeconds -= 1,
+	totalSeconds > 0 ? (document.getElementById("continue").style.display = "block", document.getElementById("continue").value = "Please wait.. " + totalSeconds, getTiming()) : 0 == totalSeconds && (document.getElementById("continue").value = "Continue", document.getElementById("continue").onclick = function(){var e = Base64.decode($_GET("url"));location.href = e})
+}
+var totalTimeout = 0;
+var totalSeconds = 6;
